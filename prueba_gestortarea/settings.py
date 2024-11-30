d6ce8555c9b7f20a7915cbb3c1d52a7d5643764f
leo <sofialespinola@gmail.com>
2024-11-29 21:27:01 -0300

    tercer commit

--- prueba_gestortarea/settings.py
+++ prueba_gestortarea/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = config('SECRET_KEY_env')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
